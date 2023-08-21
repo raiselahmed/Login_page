b5788779eb19b0c61a3e86b6062bcbcef63179f5
Raisel <raiselahmed985@gmail.com>
2023-08-21 23:44:19 +0600

    ok

---
+++ assests/js/app.js
@@ -0,0 +1,8 @@
+// JavaScript code
+document.getElementById('myButton').addEventListener('click', function() {
+    showAlert();
+});
+
+function showAlert() {
+    alert('Relax and try to remember your password')
+}
